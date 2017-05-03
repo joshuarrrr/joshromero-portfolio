5c98f716cef54b497179c37164b507aaedd2c9cf
Josh Romero <josh@joshromero.com>
2017-05-03 09:47:39 -0400

    add draft of Boston map post

---
+++ source/_posts/Tracking-Boston-s-housing-and-income-by-Transit.md
@@ -0,0 +1,38 @@
+title: Tracking Boston's housing and income by transit
+date: 2017-05-02 18:50:33
+tags:
+---
+Hello World!<!-- more --> 
+{% raw %}
+<link rel="stylesheet" href="assets/css/leaflet.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/qgis2web.css">
+<link rel="stylesheet" href="assets/css/MarkerCluster.css" />
+<link rel="stylesheet" href="assets/css/MarkerCluster.Default.css" />
+<link rel="stylesheet" href="assets/css/leaflet-search.css" />
+<link rel="stylesheet" href="assets/css/custom.css" />
+<style>
+#map {
+    width: 100%;
+    height: 500px;
+}
+</style>
+
+<div id="map">
+</div>
+<script src="assets/js/qgis2web_expressions.js"></script>
+<script src="assets/js/leaflet.js"></script>
+<script src="assets/js/leaflet-heat.js"></script>
+<script src="assets/js/leaflet.rotatedMarker.js"></script>
+<script src="assets/js/OSMBuildings-Leaflet.js"></script>
+<script src="assets/js/leaflet-hash.js"></script>
+<script src="assets/js/leaflet-tilelayer-wmts.js"></script>
+<script src="assets/js/Autolinker.min.js"></script>
+<script src="assets/js/leaflet.markercluster.js"></script>
+<script src="assets/js/leaflet-search.js"></script>
+<script src="assets/data/eighthmile0.js"></script>
+<script src="assets/data/300feet1.js"></script>
+<script src="assets/data/Lines2.js"></script>
+<script src="assets/data/Stops3.js"></script>
+<script src="assets/data/centralpoint4.js"></script>
+<script src="assets/js/map.js"></script>
+{% endraw %}
---
+++ source/_posts/Tracking-Boston-s-housing-and-income-by-Transit/assets/js/map.js
@@ -0,0 +1,533 @@
+var highlightLayer;
+
+function highlightFeature(e) {
+    highlightLayer = e.target;
+    highlightLayer.openPopup();
+}
+L.ImageOverlay.include({
+    getBounds: function() {
+        return this._bounds;
+    }
+});
+
+var isMobile = false;
+var isDesktop = false;
+var isHuge = false;
+
+function determineSize() {
+    var viewportWidth = window.innerWidth;
+    
+    if (viewportWidth < 1024) {
+        isMobile = true;
+    } else if (viewportWidth >= 1024 && viewportWidth < 1600) {
+        isDesktop = true;
+    }
+    else {
+        isHuge = true;
+    }
+}
+
+determineSize();
+
+
+var center = new L.LatLng(42.3171, -71.1046);
+var map = L.map('map', {
+    zoomControl: true,
+    // center: center,
+    maxZoom: 16,
+    minZoom: 11
+    // zoom: 11
+}).fitBounds([
+    [42.40, -71.15],
+    [42.30, -70.98]
+]);
+
+map.panTo(new L.LatLng(42.3574, -71.0601));
+
+var hash = new L.Hash(map);
+map.attributionControl.addAttribution('<a href="https://github.com/tomchadwin/qgis2web" target="_blank">qgis2web</a>');
+var bounds_group = new L.featureGroup([]);
+var basemap0 = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 16
+});
+basemap0.addTo(map);
+
+function setBounds() {}
+
+function geoJson2heat(geojson, weight) {
+    return geojson.features.map(function(feature) {
+        return [
+            feature.geometry.coordinates[1],
+            feature.geometry.coordinates[0],
+            feature.properties[weight]
+        ];
+    });
+}
+
+function pop_eighthmile0(feature, layer) {
+    // layer.on({
+    //     mouseout: function(e) {
+    //         if (typeof layer.closePopup == 'function') {
+    //             layer.closePopup();
+    //         } else {
+    //             layer.eachLayer(function(feature) {
+    //                 feature.closePopup()
+    //             });
+    //         }
+    //     },
+    //     mouseover: highlightFeature,
+    // });
+    // var popupContent = '<table>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['STATION'] !== null ? Autolinker.link(String(feature.properties['STATION'])) : '') + '</td>\
+    //                 </tr>\
+    //             </table>';
+    // layer.bindPopup(popupContent);
+}
+
+function style_eighthmile0() {
+    return {
+        pane: 'pane_eighthmile0',
+        opacity: 1,
+        color: 'rgba(0,0,0,0.45)',
+        dashArray: '10,5',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 2.0,
+        fillOpacity: 1,
+        fillColor: 'rgba(0,0,0,0.0)',
+    }
+}
+map.createPane('pane_eighthmile0');
+map.getPane('pane_eighthmile0').style.zIndex = 400;
+map.getPane('pane_eighthmile0').style['mix-blend-mode'] = 'normal';
+var layer_eighthmile0 = new L.geoJson(json_eighthmile0, {
+    attribution: '<a href=""></a>',
+    pane: 'pane_eighthmile0',
+    onEachFeature: pop_eighthmile0,
+    style: style_eighthmile0
+});
+bounds_group.addLayer(layer_eighthmile0);
+map.addLayer(layer_eighthmile0);
+
+function pop_300feet1(feature, layer) {
+    // layer.on({
+    //     mouseout: function(e) {
+    //         if (typeof layer.closePopup == 'function') {
+    //             layer.closePopup();
+    //         } else {
+    //             layer.eachLayer(function(feature) {
+    //                 feature.closePopup()
+    //             });
+    //         }
+    //     },
+    //     mouseover: highlightFeature,
+    // });
+    // var popupContent = '<table>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['STATION'] !== null ? Autolinker.link(String(feature.properties['STATION'])) : '') + '</td>\
+    //                 </tr>\
+    //             </table>';
+    // layer.bindPopup(popupContent);
+}
+
+function style_300feet1() {
+    return {
+        pane: 'pane_300feet1',
+        opacity: 1,
+        color: 'rgba(0,0,0,0.45)',
+        dashArray: '10,5',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 2.0,
+        fillOpacity: 1,
+        fillColor: 'rgba(0,0,0,0.0)',
+    }
+}
+map.createPane('pane_300feet1');
+map.getPane('pane_300feet1').style.zIndex = 401;
+map.getPane('pane_300feet1').style['mix-blend-mode'] = 'normal';
+var layer_300feet1 = new L.geoJson(json_300feet1, {
+    attribution: '<a href=""></a>',
+    pane: 'pane_300feet1',
+    onEachFeature: pop_300feet1,
+    style: style_300feet1
+});
+bounds_group.addLayer(layer_300feet1);
+map.addLayer(layer_300feet1);
+
+function pop_Lines2(feature, layer) {
+    // layer.on({
+    //     mouseout: function(e) {
+    //         if (typeof layer.closePopup == 'function') {
+    //             layer.closePopup();
+    //         } else {
+    //             layer.eachLayer(function(feature) {
+    //                 feature.closePopup()
+    //             });
+    //         }
+    //     },
+    //     mouseover: highlightFeature,
+    // });
+    // var popupContent = '<table>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['LINE'] !== null ? Autolinker.link(String(feature.properties['LINE'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['ROUTE'] !== null ? Autolinker.link(String(feature.properties['ROUTE'])) : '') + '</td>\
+    //                 </tr>\
+    //             </table>';
+    // layer.bindPopup(popupContent);
+}
+
+function style_Lines2(feature) {
+    switch (feature.properties['LINE']) {
+        case 'BLUE':
+            return {
+                pane: 'pane_Lines2',
+                opacity: 1,
+                color: 'rgba(31,120,180,1.0)',
+                dashArray: '',
+                lineCap: 'square',
+                lineJoin: 'bevel',
+                weight: 6.0,
+                fillOpacity: 0,
+            }
+            break;
+        case 'GREEN':
+            return {
+                pane: 'pane_Lines2',
+                opacity: 1,
+                color: 'rgba(51,160,44,1.0)',
+                dashArray: '',
+                lineCap: 'square',
+                lineJoin: 'bevel',
+                weight: 6.0,
+                fillOpacity: 0,
+            }
+            break;
+        case 'ORANGE':
+            return {
+                pane: 'pane_Lines2',
+                opacity: 1,
+                color: 'rgba(255,127,0,1.0)',
+                dashArray: '',
+                lineCap: 'square',
+                lineJoin: 'bevel',
+                weight: 6.0,
+                fillOpacity: 0,
+            }
+            break;
+        case 'RED':
+            return {
+                pane: 'pane_Lines2',
+                opacity: 1,
+                color: 'rgba(227,26,28,1.0)',
+                dashArray: '',
+                lineCap: 'square',
+                lineJoin: 'bevel',
+                weight: 6.0,
+                fillOpacity: 0,
+            }
+            break;
+    }
+}
+map.createPane('pane_Lines2');
+map.getPane('pane_Lines2').style.zIndex = 402;
+map.getPane('pane_Lines2').style['mix-blend-mode'] = 'normal';
+var layer_Lines2 = new L.geoJson(json_Lines2, {
+    attribution: '<a href=""></a>',
+    pane: 'pane_Lines2',
+    onEachFeature: pop_Lines2,
+    style: style_Lines2
+});
+bounds_group.addLayer(layer_Lines2);
+map.addLayer(layer_Lines2);
+
+function pop_Stops3(feature, layer) {
+    layer.on({
+        mouseout: function(e) {
+            // if (typeof layer.closePopup == 'function') {
+            //     layer.closePopup();
+            // } else {
+            //     layer.eachLayer(function(feature) {
+            //         feature.closePopup()
+            //     });
+            // }
+        },
+        mouseover: highlightFeature,
+    });
+    var popupContent = '<div><h2 class="' + feature.properties['LINE'].toLowerCase().replace(/\//g, '-') + '">' + (feature.properties['STATION'] !== null ? Autolinker.link(String(feature.properties['STATION'])) : '') + '</h2>\
+                    <div class="' + feature.properties['LINE'].toLowerCase().replace(/\//g, '-') + '">' + (feature.properties['ROUTE'] !== null ? Autolinker.link(String(feature.properties['ROUTE'])) : '') + '</div>\
+                    <table class="stats-table">\
+                    <tr>\
+                        <th>Distance</th>\
+                        <th class="number-col">300 ft</th>\
+                        <th class="number-col">1/8 mile</th>\
+                    </tr>\
+                    </tr>\
+                        <td>Income</td>\
+                        <td class="number-col">' + (feature.properties['medIncCalc'] !== null ? '$' + Autolinker.link(feature.properties['medIncCalc'].toLocaleString()) : '-') + '</td>\
+                        <td class="number-col">' + (feature.properties['eighth_mic'] !== null ? '$' + Autolinker.link(feature.properties['eighth_mic'].toLocaleString()) : '-') + '</td>\
+                    </tr>\
+                    <tr>\
+                        <td>Gross Rent</td>\
+                        <td class="number-col">' + (feature.properties['MedRenCalc'] !== null ? '$' + Autolinker.link(feature.properties['MedRenCalc'].toLocaleString()) : '-') + '</td>\
+                        <td class="number-col">' + (feature.properties['eighth_mrc'] !== null ? '$' + Autolinker.link(feature.properties['eighth_mrc'].toLocaleString()) : '-') + '</td>\
+                    </tr>\
+                    <tr>\
+                        <td>House Value</td>\
+                        <td class="number-col">' + (feature.properties['MedHomCalc'] !== null ? '$' + Autolinker.link(feature.properties['MedHomCalc'].toLocaleString()) : '-') + '</td>\
+                        <td class="number-col">' + (feature.properties['eighth_mhc'] !== null ? '$' + Autolinker.link(feature.properties['eighth_mhc'].toLocaleString()) : '-') + '</td>\
+                    </tr>\
+                </table>';
+    layer.bindPopup(popupContent, { maxWidth: 234 });
+}
+
+function style_Stops3(feature) {
+    switch (feature.properties['LINE']) {
+        case 'BLUE':
+            return {
+                pane: 'pane_Stops3',
+                radius: 4.0,
+                opacity: 1,
+                color: 'rgba(255,255,255,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(121,175,210,1.0)',
+            }
+            break;
+        case 'BLUE/GREEN':
+            return {
+                pane: 'pane_Stops3',
+                radius: 6.0,
+                opacity: 1,
+                color: 'rgba(0,0,0,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(191,191,191,1.0)',
+            }
+            break;
+        case 'BLUE/ORANGE':
+            return {
+                pane: 'pane_Stops3',
+                radius: 6.0,
+                opacity: 1,
+                color: 'rgba(0,0,0,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(191,191,191,1.0)',
+            }
+            break;
+        case 'GREEN':
+            return {
+                pane: 'pane_Stops3',
+                radius: 4.0,
+                opacity: 1,
+                color: 'rgba(255,255,255,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(51,160,44,1.0)',
+            }
+            break;
+        case 'GREEN/ORANGE':
+            return {
+                pane: 'pane_Stops3',
+                radius: 6.0,
+                opacity: 1,
+                color: 'rgba(0,0,0,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(191,191,191,1.0)',
+            }
+            break;
+        case 'GREEN/RED':
+            return {
+                pane: 'pane_Stops3',
+                radius: 6.0,
+                opacity: 1,
+                color: 'rgba(0,0,0,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(191,191,191,1.0)',
+            }
+            break;
+        case 'ORANGE':
+            return {
+                pane: 'pane_Stops3',
+                radius: 4.0,
+                opacity: 1,
+                color: 'rgba(255,255,255,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(255,127,0,1.0)',
+            }
+            break;
+        case 'ORANGE/RED':
+            return {
+                pane: 'pane_Stops3',
+                radius: 6.0,
+                opacity: 1,
+                color: 'rgba(0,0,0,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(191,191,191,1.0)',
+            }
+            break;
+        case 'RED':
+            return {
+                pane: 'pane_Stops3',
+                radius: 4.0,
+                opacity: 1,
+                color: 'rgba(255,255,255,1.0)',
+                dashArray: '',
+                lineCap: 'butt',
+                lineJoin: 'miter',
+                weight: 2.0,
+                fillOpacity: 1,
+                fillColor: 'rgba(227,26,28,1.0)',
+            }
+            break;
+    }
+}
+map.createPane('pane_Stops3');
+map.getPane('pane_Stops3').style.zIndex = 403;
+map.getPane('pane_Stops3').style['mix-blend-mode'] = 'normal';
+var layer_Stops3 = new L.geoJson(json_Stops3, {
+    attribution: '<a href=""></a>',
+    pane: 'pane_Stops3',
+    onEachFeature: pop_Stops3,
+    pointToLayer: function(feature, latlng) {
+        var context = {
+            feature: feature,
+            variables: {}
+        };
+        return L.circleMarker(latlng, style_Stops3(feature))
+    }
+});
+bounds_group.addLayer(layer_Stops3);
+map.addLayer(layer_Stops3);
+
+function pop_centralpoint4(feature, layer) {
+    // layer.on({
+    //     mouseout: function(e) {
+    //         if (typeof layer.closePopup == 'function') {
+    //             layer.closePopup();
+    //         } else {
+    //             layer.eachLayer(function(feature) {
+    //                 feature.closePopup()
+    //             });
+    //         }
+    //     },
+    //     mouseover: highlightFeature,
+    // });
+    // var popupContent = '<table>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['STATION'] !== null ? Autolinker.link(String(feature.properties['STATION'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['LINE'] !== null ? Autolinker.link(String(feature.properties['LINE'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['TERMINUS'] !== null ? Autolinker.link(String(feature.properties['TERMINUS'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['ROUTE'] !== null ? Autolinker.link(String(feature.properties['ROUTE'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['area'] !== null ? Autolinker.link(String(feature.properties['area'])) : '') + '</td>\
+    //                 </tr>\
+    //                 <tr>\
+    //                     <td colspan="2">' + (feature.properties['perim'] !== null ? Autolinker.link(String(feature.properties['perim'])) : '') + '</td>\
+    //                 </tr>\
+    //             </table>';
+    // layer.bindPopup(popupContent);
+}
+
+function style_centralpoint4() {
+    return {
+        pane: 'pane_centralpoint4',
+        radius: 4.0,
+        opacity: 1,
+        color: 'rgba(0,0,0,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1,
+        fillOpacity: 1,
+        fillColor: 'rgba(182,22,148,1.0)',
+    }
+}
+map.createPane('pane_centralpoint4');
+map.getPane('pane_centralpoint4').style.zIndex = 404;
+map.getPane('pane_centralpoint4').style['mix-blend-mode'] = 'normal';
+var layer_centralpoint4 = new L.geoJson(json_centralpoint4, {
+    attribution: '<a href=""></a>',
+    pane: 'pane_centralpoint4',
+    onEachFeature: pop_centralpoint4,
+    pointToLayer: function(feature, latlng) {
+        var context = {
+            feature: feature,
+            variables: {}
+        };
+        return L.circleMarker(latlng, style_centralpoint4(feature))
+    }
+});
+bounds_group.addLayer(layer_centralpoint4);
+map.addLayer(layer_centralpoint4);
+var title = new L.Control();
+title.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+title.update = function() {
+    this._div.innerHTML = '<h2>Income and Housing by T Stop</h2>';
+};
+if ( !isMobile ) {
+    title.addTo(map);
+}
+setBounds();
+if (map.hasLayer(layer_Stops3)) {
+    if (map.getZoom() <= 19 && map.getZoom() >= 10) {
+        layer_Stops3.eachLayer(function(layer) {
+            layer.openTooltip();
+        });
+    } else {
+        layer_Stops3.eachLayer(function(layer) {
+            layer.closeTooltip();
+        });
+    }
+}
+map.addControl(new L.Control.Search({
+    layer: layer_Stops3,
+    initial: false,
+    hideMarkerOnCollapse: true,
+    propertyName: 'STATION',
+    textPlaceholder: "Search for Station..."
+}));
